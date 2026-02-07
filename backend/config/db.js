6857b271515a6cf29540038df2fd5af0f9c16213
alakh niranjan <alakh.kushwaha1@gmil.com>
2026-02-08 05:28:35 +0530

    Delete all uploaded folders

--- backend/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const connectDB = async () => {
-  await mongoose.connect("mongodb://127.0.0.1:27017/fooddelivery");
-  console.log("MongoDB connected successfully");
-};
-
-export default connectDB;
